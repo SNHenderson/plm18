4a018410ec4188740ecd49bc95a99be181deeb97
Joshua Kayani <jkayani@ncsu.edu>
2018-03-08 22:12:00 -0500

    Added fix to create non-existent directories for logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,11 +1,18 @@
+import os
+
 class Logger(object):
-	def __init__(self, file):
-		self.file_name = file
-		self.file = open(file, 'w')
+    def __init__(self, file):
 
-	def print(self, *args):
-		print(*args)
-		print(*args, file = self.file)
+        # Makes any necessary directories (i.e, logs/)
+        dirname = os.path.dirname(file)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        self.file_name = file
+        self.file = open(file, 'w')
 
-	def close(self):
-		self.file.close()
+    def print(self, *args):
+        print(*args)
+        print(*args, file = self.file)
+
+    def close(self):
+        self.file.close()
